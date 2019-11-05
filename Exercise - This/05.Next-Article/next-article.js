468dff36c90de14b601c8537f5f48083a8d8bd05
Бойко Боев <boykopetevboev@gmail.com>
2019-11-05 18:09:20 +0200

    Update next-article.js

--- Exercise - This/05.Next-Article/next-article.js
+++ Exercise - This/05.Next-Article/next-article.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    return('dfdfd')
+    const mainDiv = document.getElementById('content');
+    let index = 0;
+    return function(){
+        if(index < articles.length){
+            const article = document.createElement('article');
+            article.textContent = articles[index];
+            mainDiv.appendChild(article);
+            index++;
+        }
+    }
 }
