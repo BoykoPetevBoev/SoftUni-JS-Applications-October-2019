7b13995c6f4cf97945de0b28f98e73b967c04e9c
Бойко Боев <boykopetevboev@gmail.com>
2019-11-05 19:18:46 +0200

    Update 05.String Extension.js

--- Exercise - Object Composition/05.String Extension.js
+++ Exercise - Object Composition/05.String Extension.js
@@ -1,44 +1,40 @@
-String.prototype.ensureStart = function (str) {
-    if (this.startsWith(str)) {
-        return `${str}${this}`;
+(function () {
+    String.prototype.ensureStart = function (str) {
+        if (!this.startsWith(str)) {
+            return `${str}${this}`;
+        }
+        return `${this}`;
     }
-    return `${this}`;
-}
-String.prototype.ensureEnd = function (str) {
-    if (this.endsWith(str)) {
-        return `${this}${str}`;
+    String.prototype.ensureEnd = function (str) {
+        if (!this.endsWith(str)) {
+            return `${this}${str}`;
+        }
+        return `${this}`;
     }
-    return `${this}`;
-}
-
-String.prototype.isEmpty = function () {
-    return this.toString() === 0;
-}
-String.prototype.truncate = function (n) {
-    if (n < 4) {
-
+    String.prototype.isEmpty = function () {
+        return this.toString() === '';
     }
-    if (n >= this.length) {
-
+    String.prototype.truncate = function (n) {
+        if (n < 4) {
+            return '.'.repeat(n);
+        }
+        else if (n >= this.length) {
+            return this.toString();
+        }
+        else if (n < this.length) {
+            let lastSpace = this.substr(0, n - 2).lastIndexOf(' ');
+            if (lastSpace !== -1) {
+                return this.substr(0, lastSpace).concat('...');
+            }
+            else {
+                return this.substr(0, n - 3).concat('...');
+            }
+        }
     }
-}
-String.prototype.format = function (string, ...params) {
-
-}
-
-    (function () {
-
-    }())
-
-
-let str = 'my string';
-str = str.ensureStart('my');
-str = str.ensureStart('hello ');
-str = str.truncate(16);
-str = str.truncate(14);
-str = str.truncate(8);
-str = str.truncate(4);
-str = str.truncate(2);
-str = String.format('The {0} {1} fox', 'quick', 'brown');
-str = String.format('jumps {0} {1}', 'dog');
-
+    String.format = function (string, ...params) {
+        return params
+            .reduce((str, param, i) => {
+                return str.replace(`{${i}}`, param);
+            }, string);
+    }
+}())
