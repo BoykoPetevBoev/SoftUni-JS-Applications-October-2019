69df7714679b7522a535bfd8045906b5ae125c10
Бойко Боев <boykopetevboev@gmail.com>
2019-11-06 17:40:59 +0200

    Create 01.Balloons.js

---
+++ Exercise - Prototypes and Inheritance/01.Balloons.js
@@ -0,0 +1,36 @@
+function solve() {
+    class Balloon {
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = gasWeight;
+        }
+    }
+    class PartyBalloon extends Balloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight);
+            this.ribbonColor = ribbonColor;
+            this.ribbonLength = ribbonLength;
+            this._ribbon = {
+                color: this.ribbonColor,
+                length: this.ribbonLength
+            }
+        }
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+        get text(){
+            return this._text;
+        }
+    }
+    return {
+        Balloon,
+        PartyBalloon,
+        BirthdayBalloon
+    }
+}
