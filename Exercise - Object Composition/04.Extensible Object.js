5c231b230b1f30bcdde18b65501983d59d1e4824
Бойко Боев <boykopetevboev@gmail.com>
2019-11-07 12:20:17 +0200

    Update 04.Extensible Object.js

--- Exercise - Object Composition/04.Extensible Object.js
+++ Exercise - Object Composition/04.Extensible Object.js
@@ -1,5 +1,14 @@
-function solve(input) {
-    
+function solve() {
+    let obj = {
+        extend: function (template) {
+            Object
+                .keys(template)
+                .forEach(parentProp => {
+                    typeof (template[parentProp]) == "function"
+                        ? Object.getPrototypeOf(obj)[parentProp] = template[parentProp]
+                        : obj[parentProp] = template[parentProp];
+                });
+        }
+    }
+    return obj;
 }
-
-solve();
