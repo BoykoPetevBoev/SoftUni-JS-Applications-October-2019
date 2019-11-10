f0a282f28251becb5203768a0647ff1d42d5538c
Бойко Боев <boykopetevboev@gmail.com>
2019-11-10 12:35:31 +0200

    Create app.js

---
+++ Exercise - REST Services and AJAX/04.Messenger/app.js
@@ -0,0 +1,40 @@
+function attachEvents() {
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const inputName = document.getElementById('author');
+    const inputMessage = document.getElementById('content');
+    const textarea = document.getElementById('messages');
+
+    sendBtn.addEventListener('click', sendMessage);
+    refreshBtn.addEventListener('click', showMessages);
+
+    function sendMessage() {
+        const headers = {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                author: inputName.value,
+                content: inputMessage.value
+            })
+        }
+        fetch('https://rest-messanger.firebaseio.com/messanger.json', headers)
+            .catch(handleErrors);
+    }
+    function showMessages() {
+        fetch('https://rest-messanger.firebaseio.com/messanger.json')
+            .then(res => res.json())
+            .then(data => {
+                textarea.value = '';
+                Object.values(data)
+                    .forEach(({ author, content }) => {
+                        textarea.value += `${author}: ${content}\n`;
+                    });
+            })
+            .catch(handleErrors);
+    }
+    function handleErrors(err) {
+        console.log(err)
+    }
+}
+
+attachEvents();
