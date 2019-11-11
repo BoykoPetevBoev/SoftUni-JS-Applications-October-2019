6a55586b6451b8ffca82385e6bc654fe84fce230
Бойко Боев <boykopetevboev@gmail.com>
2019-11-11 22:25:10 +0200

    Update app.js

--- Exercise - REST Services and AJAX/03.Phonebook/app.js
+++ Exercise - REST Services and AJAX/03.Phonebook/app.js
@@ -9,33 +9,36 @@ function attachEvents() {
     btnCreate.addEventListener('click', addNewInfo);
 
     function deleteData() {
-            let headers = {
-                method: 'DELETE'
-            }
-            fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${this.value}.json`, headers)
-                .then(loadData)
-                .catch(handleError)
+        let headers = {
+            method: 'DELETE'
+        }
+        fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${this.value}.json`, headers)
+            .then(loadData)
+            .catch(handleError)
     }
     function loadData() {
         fetch('https://phonebook-nakov.firebaseio.com/phonebook.json')
+            .then(chechForErrors)
             .then(res => res.json())
-            .then(data => {
-                ul.innerHTML = '';
-                Object.entries(data)
-                    .forEach(([id, info]) => {
-                        let li = document.createElement('li');
-                        li.textContent = `${info.person}: ${info.phone}`;
-
-                        let btn = document.createElement('button');
-                        btn.textContent = 'Delete';
-                        btn.value = id;
-                        btn.addEventListener('click', deleteData)
-
-                        li.appendChild(btn);
-                        ul.appendChild(li);
-                    })
-            })
+            .then(chechForErrors)
+            .then(printInfo)
             .catch(handleError)
+        function printInfo(data) {
+            ul.innerHTML = '';
+            Object.entries(data)
+                .forEach(([id, info]) => {
+                    let li = document.createElement('li');
+                    li.textContent = `${info.person}: ${info.phone}`;
+
+                    let btn = document.createElement('button');
+                    btn.textContent = 'Delete';
+                    btn.value = id;
+                    btn.addEventListener('click', deleteData)
+
+                    li.appendChild(btn);
+                    ul.appendChild(li);
+                })
+        }
     }
     function addNewInfo() {
         let headers = {
@@ -54,8 +57,17 @@ function attachEvents() {
             })
             .catch(handleError)
     }
-    function handleError() {
-        console.error
+    function chechForErrors(res) {
+        if (res === null || res === undefined) {
+            throw new Error('Invalid JSON data!');
+        }
+        else if (res.ok === false) {
+            throw new Error(`${res.status} - ${res.statusText}`);
+        }
+        return res;
+    }
+    function handleError(err) {
+        console.error(err);
     }
 }
 attachEvents();
