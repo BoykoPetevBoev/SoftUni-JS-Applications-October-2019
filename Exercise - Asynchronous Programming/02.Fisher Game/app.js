72b67692912f7d081c0d9e9df2e348157d9f90f0
Бойко Боев <boykopetevboev@gmail.com>
2019-11-15 13:49:25 +0200

    Update app.js

--- Exercise - Asynchronous Programming/02.Fisher Game/app.js
+++ Exercise - Asynchronous Programming/02.Fisher Game/app.js
@@ -1,3 +1,4 @@
+import { fetchData } from './fetchRequest.js';
 function attachEvents() {
     class Catch {
         constructor(angler, weight, species, location, bait, captureTime) {
@@ -9,20 +10,21 @@ function attachEvents() {
             this.captureTime = captureTime;
         }
     }
-    const buttons = [
-        ...document.getElementsByClassName('update'),
-        ...document.getElementsByClassName('delete'),
-        ...document.getElementsByClassName('load'),
-        ...document.getElementsByClassName('add')
-    ]
+    const deleteBtn = () => document.getElementsByClassName('delete');
+    const updateBtn = () => document.getElementsByClassName('update');
+    const loadBtn = () => document.getElementsByClassName('load');
+    const addBtn = () => document.getElementsByClassName('add');
+    const buttons = [...deleteBtn(), ...updateBtn(), ...loadBtn(), ...addBtn()];
+
     const buttonsFunctionality = {
         Add: () => createCatch(),
         Load: () => loadCatch(),
         Update: (btn) => updateCatch(btn),
         Delete: (btn) => deleteCatch(btn)
     }
-    const catchForm = document.getElementById('addForm');
-    const mainDiv = document.getElementById('catches');
+    const $catchForm = document.getElementById('addForm');
+    const $mainDiv = document.getElementById('catches');
+    const $template = $mainDiv.children[0];
 
     const baseURL = 'https://fisher-game.firebaseio.com/catches';
 
@@ -36,74 +38,42 @@ function attachEvents() {
         fetchData(url, headers).then(extractData);
     }
     function extractData(data) {
+        $mainDiv.innerHTML = '';
         Object.keys(data).forEach(code => {
-            createDomPart(code, data[code]);
+            fillInfo(code, data[code]);
         });
+        buttons.push(...deleteBtn(), ...updateBtn());
     }
-    function createDomPart(code, obj) {
-        const x = undefined;
-        const divConteiner = createElement('div', x, 'catch');
-        divConteiner.setAttribute('data-id', code);
-
-        const arr = [];
-        arr.push(createElement('lable', x, x, x, 'Angler'));
-        arr.push(createElement('input', obj.angler, 'angler', 'text'));
-        arr.push(createElement('hr'));
-        arr.push(createElement('lable', x, x, x, 'Weight'));
-        arr.push(createElement('input', obj.weight, 'weight', 'number'));
-        arr.push(createElement('hr'));
-        arr.push(createElement('lable', x, x, x, 'Species'));
-        arr.push(createElement('input', obj.species, 'species', 'text'));
-        arr.push(createElement('hr'));
-        arr.push(createElement('lable', x, x, x, 'Location'));
-        arr.push(createElement('input', obj.location, 'location', 'text'));
-        arr.push(createElement('hr'));
-        arr.push(createElement('lable', x, x, x, 'Bait'));
-        arr.push(createElement('input', obj.bait, 'bait', 'text'));
-        arr.push(createElement('hr'));
-        arr.push(createElement('lable', x, x, x, 'Capture Time'));
-        arr.push(createElement('input', obj.captureTime, 'captureTime', 'number'));
-        arr.push(createElement('hr'));
-        arr.push(createElement('button', x, 'update', x, 'Update'));
-        arr.push(createElement('button', x, 'delete', x, 'Delete'));
-
-        appendChilds(divConteiner, arr);
+    function fillInfo(code, obj) {
+        let domElement = $template.cloneNode(true);
+        domElement.setAttribute('data-id', code);
+        Array.from(domElement.children).map(el => {
+            if (el.tagName === 'INPUT') {
+                el.value = obj[el.className]
+            }
+        })
+        appendDomElement($mainDiv, domElement);
     }
-    function appendChilds(parent, childs) {
-        
-        parent.append(...childs);
-        mainDiv.appendChild(parent);
-    }
-    function createElement(domElement, value, className, type, textContent) {
-        const element = document.createElement(domElement);
-        if (className !== undefined) {
-            element.classList.add(className);
-        }
-        if (value !== undefined) {
-            element.value = value;
-        }
-        if (type !== undefined) {
-            element.type = type;
-        }
-        if (textContent !== undefined) {
-            element.textContent = textContent;
-        }
-        return element;
+    function appendDomElement(parent, childs) {
+        parent.appendChild(childs);
     }
     function createCatch() {
         const url = `${baseURL}.json`;
-        createRequestInfo(url, 'POST', catchForm);
+        createRequestInfo(url, 'POST', $catchForm);
+        loadCatch();
     }
     function updateCatch(btn) {
         const code = btn.parentNode.getAttribute("data-id");
         const url = `${baseURL}/${code}.json`;
         createRequestInfo(url, 'PUT', btn.parentNode);
+        loadCatch();
     }
     function deleteCatch(btn) {
         const headers = { method: 'DELETE' };
         const code = btn.parentNode.getAttribute("data-id");
         const url = `${baseURL}/${code}.json`;
         fetchData(url, headers);
+        loadCatch();
     }
     function createRequestInfo(url, method, parent) {
         const values = getInfo(parent);
@@ -115,23 +85,13 @@ function attachEvents() {
         fetchData(url, headers);
     }
     function getInfo(div) {
-        return Array.from(div.getElementsByTagName('input'))
+        return Array
+            .from(div.getElementsByTagName('input'))
             .reduce((arr, input) => {
                 arr.push(input.value);
                 return arr;
             }, []);
     }
-    async function fetchData(url, headers) {
-        try {
-            const res = await fetch(url, headers);
-            console.log(res)
-            const data = await res.json();
-            return data;
-        }
-        catch{
-            console.error;
-        }
-    }
 }
-
 attachEvents();
+
