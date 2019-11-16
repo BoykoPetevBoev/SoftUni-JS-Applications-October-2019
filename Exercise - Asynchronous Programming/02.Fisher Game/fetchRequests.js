7e6d53ec9b49702df20460e5368b3ef94d70319e
Бойко Боев <boykopetevboev@gmail.com>
2019-11-16 18:23:50 +0200

    Update fetchRequests.js

--- Exercise - Asynchronous Programming/02.Fisher Game/fetchRequests.js
+++ Exercise - Asynchronous Programming/02.Fisher Game/fetchRequests.js
@@ -1,11 +1,41 @@
-export async function fetchData(url, headers) {
-    try {
-        const res = await fetch(url, headers);
-        console.log(`Response: ${res.status} - ${res.statusText}`);
-        const data = await res.json();
-        return data;
+export function fetchData() {
+    const baseURL = 'https://fisher-game.firebaseio.com/catches';
+    const get = () => {
+        const url = `${baseURL}.json`;
+        const headers = { method: 'GET' }
+        return fetch(url, headers).then(handleErrors).then(res => res.json());
+    };
+    const post = (body) => {
+        const url = `${baseURL}.json`;
+        const headers = {
+            method: 'POST',
+            body: JSON.stringify(body)
+        }
+        return fetch(url, headers).then(handleErrors).then(res => res.json());
+    };
+    const put = (body, code) => {
+        const url = `${baseURL}/${code}.json`;
+        const headers = {
+            method: 'PUT',
+            body: JSON.stringify(body)
+        };
+        return fetch(url, headers).then(handleErrors).then(res => res.json());
     }
-    catch{
-        console.error;
+    const del = (code) =>{
+        const url = `${baseURL}/${code}.json`;
+        const headers = { method: 'DELETE' };
+        return fetch(url, headers).then(handleErrors).then(res => res.json());
+    }
+    function handleErrors(res){
+        if(!res.ok){
+            throw new Error(`${res.status} - ${res.statusText}`);
+        }
+        return res;
+    }
+    return {
+        get,
+        post,
+        put,
+        del
     }
 }
