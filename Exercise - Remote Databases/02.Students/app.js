825e425896547216aaa6b67999fd7a54f71f738c
Бойко Боев <boykopetevboev@gmail.com>
2019-11-19 13:33:55 +0200

    Update app.js

--- Exercise - Remote Databases/02.Students/app.js
+++ Exercise - Remote Databases/02.Students/app.js
@@ -1,11 +1,43 @@
 import { get, post, put, del } from './requests.js';
 
-get().then(processData)
+let allIds = [];
+const body = () => document.querySelector("#results > tbody");
 
+document.getElementById('submit').addEventListener('click', addNewInfo)
+function addNewInfo(e) {
+    e.preventDefault();
+    const inputs = {
+        firstName: () => document.getElementById('firstName'),
+        lastName: () => document.getElementById('lastName'),
+        facNumber: () => document.getElementById('facNumber'),
+        grade: () => document.getElementById('grade'),
+        id: () => document.getElementById('id')
+    }
+    if (isDataValid(inputs)) {
+        const body = {
+            FacultyNumber: inputs.facNumber().value,
+            FirstName: inputs.firstName().value,
+            Grade: inputs.grade().value,
+            ID: inputs.id().value,
+            LastName: inputs.lastName().value
+        }
+        post(body).then(printElements);
+        clearInputs(inputs);
+    }
+}
+function printElements() {
+    get().then(processData)
+}
+printElements();
 function processData(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error('Invalid data!');
+    }
+    allIds = [];
+    body().innerHTML = '';
+
     const sortedData = sortData(arr);
     sortedData.forEach(element => {
-        console.log(element)
         createRow(element)
     });
 }
@@ -15,25 +47,46 @@ function sortData(arr) {
     return result;
 }
 function createRow(obj) {
+    allIds.push(obj.ID)
+
     const tr = createNewElement('tr');
     const tdID = createNewElement('td', obj.ID);
-    const tdFName = createNewElement('td', obj.FirstName);  
+    const tdFName = createNewElement('td', obj.FirstName);
     const tdLName = createNewElement('td', obj.LastName);
-    const tdFNumber = createNewElement('td',obj.FacultyNumber);
-    const tdGrade = createNewElement('td',obj.Grade);
- 
+    const tdFNumber = createNewElement('td', obj.FacultyNumber);
+    const tdGrade = createNewElement('td', obj.Grade);
+
     tr.append(tdID, tdFName, tdLName, tdFNumber, tdGrade);
-    appendNewBranch(tr);
+    appendNewBranch(body(), tr);
 }
-function createNewElement(type, text){
+function createNewElement(type, text) {
     const element = document.createElement(type);
-    if(text){
+    if (text) {
         element.textContent = text;
     }
     return element;
 }
-function appendNewBranch(element){
-    const body = document.querySelector("#results > tbody");
+function appendNewBranch(body, element) {
     body.appendChild(element);
 }
-TODO...
+function isDataValid(inputs) {
+    if (typeof inputs.firstName().value !== 'string') {
+        alert('First Name must be string!');
+    }
+    else if (typeof inputs.lastName().value !== 'string') {
+        alert('Last Name must be string!');
+    }
+    else if (Number(inputs.facNumber().value) === NaN) {
+        alert('Faculty Number must be number!');
+    }
+    else if (Number(inputs.firstName().value) === NaN) {
+        alert('Grade must be number!');
+    }
+    else if (Number(inputs.id().value) === NaN || allIds.includes(inputs.id().value)) {
+        alert("You can't use this ID!");
+    }
+    return true;
+}
+function clearInputs(inputs) {
+    Object.keys(inputs).map(key => inputs[key]().value = '');
+}
