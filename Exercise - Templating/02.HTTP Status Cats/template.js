5af4eab881ff36508d7ad8b9ed0fccc8e0733b82
Бойко Боев <boykopetevboev@gmail.com>
2019-11-23 16:58:50 +0200

    Update template.js

--- Exercise - Templating/02.HTTP Status Cats/template.js
+++ Exercise - Templating/02.HTTP Status Cats/template.js
@@ -1,8 +1,33 @@
 (() => {
-     renderCatTemplate();
+    renderCatTemplate();
+    function renderCatTemplate() {
+        const section = document.getElementById('allCats');
+        const cats = window.cats;
+        renderRequest()
+        async function renderRequest() {
+            const source = await fetch('http://127.0.0.1:5500/JS%20Applications/Exercise%20-%20Templating/02.%20HTTP%20Status%20Cats/template.hbs')
+                .then(res => res.text());
+            const template = Handlebars.compile(source);
+            const html = template({ cats });
+            section.innerHTML = html;
+        }
+    }
+})()
+function display(btn) {
+    const info = btn.parentNode.getElementsByClassName('status')[0];
+    const process = {
+        none: () => showInfo(),
+        block: () => hideInfo()
+    }
+    
+    process[info.style.display]();
 
-     function renderCatTemplate() {
-         // TODO: Render cat template and attach events
-     }
- 
-})
+    function showInfo(){
+        info.style.display = 'block';
+        btn.textContent = 'Hide status code';
+    }
+    function hideInfo(){
+        info.style.display = 'none';
+        btn.textContent = 'Show status code';
+    }
+}
