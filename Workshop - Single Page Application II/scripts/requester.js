2ec1524f8e2e8e9b52012c49202f2f50a9628138
Бойко Боев <boykopetevboev@gmail.com>
2019-12-05 18:21:55 +0200

    Add files via upload

---
+++ Workshop - Single Page Application II/scripts/requester.js
@@ -0,0 +1,59 @@
+
+const appSecret = '45a0f454217047afbe3e83e20fb50451';
+const appKey = 'kid_Hk8UcZBTB';
+const domain = `https://baas.kinvey.com`;
+
+function createAuthorization(type) {
+    return type === 'Basic'
+        ? `Basic ${btoa(`${appKey}:${appSecret}`)}`
+        : `Kinvey ${sessionStorage.getItem('authtoken')}`
+}
+function createHeaders(type, method, data) {
+    const headers = {
+        method: method,
+        headers: {
+            'Authorization': createAuthorization(type),
+            'Content-Type': 'application/json'
+        }
+    }
+    if (method === 'PUT' || method === 'POST') {
+        headers.body = JSON.stringify(data);
+    }
+    return headers;
+}
+
+
+function get(path, endpoint, type) {
+    const headers = createHeaders(type, 'GET');
+    return fetchRequest(path, endpoint, headers);
+}
+function post(path, endpoint, type, body) {
+    const headers = createHeaders(type, 'POST', body);
+    return fetchRequest(path, endpoint, headers);
+}
+function put(path, endpoint, type, body) {
+    const headers = createHeaders(type, 'PUT', body);
+    return fetchRequest(path, endpoint, headers);
+}
+function del(path, endpoint, type) {
+    const headers = createHeaders(type, 'DELETE');
+    return fetchRequest(path, endpoint, headers);
+}
+
+
+function fetchRequest(path, endpoint, headers) {
+    const url = `${domain}/${path}/${appKey}/${endpoint}`;
+    return fetch(url, headers)
+        .then(checkForErrors)
+        .then(data => data.json())
+        .catch(err => console.log(err))
+}
+function checkForErrors(req) {
+    if (!req.ok) {
+        throw new Error(`${req.status} - ${req.statusText}`);
+    }
+    return req;
+}
+export { get, post, put, del }
+
+
