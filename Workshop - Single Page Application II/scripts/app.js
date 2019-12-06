f0d0256f76ac4cb0833e7da0a30411f739ac98c3
Бойко Боев <boykopetevboev@gmail.com>
2019-12-06 15:55:52 +0200

    Update app.js

--- Workshop - Single Page Application II/scripts/app.js
+++ Workshop - Single Page Application II/scripts/app.js
@@ -25,13 +25,13 @@ const templatesPaths = {
         this.get('#/home', function (ctx) {
             getUserInfo(ctx)
             ctx.loggedIn
-            ? loadHomePage(ctx)
-            : loadPage(ctx, './templates/homePages/homePage.hbs')
+                ? loadHomePage(ctx)
+                : loadPage(ctx, './templates/homePages/homePage.hbs')
 
-            function loadHomePage(ctx){
+            function loadHomePage(ctx) {
                 get('appdata', 'events', 'Kinvey')
                     .then(res => {
-                        if(res.length === 0){
+                        if (res.length === 0) {
                             loadPage(ctx, './templates/homePages/notFound.hbs');
                         }
                         else {
@@ -45,10 +45,10 @@ const templatesPaths = {
                     })
             }
         })
-        this.get('#/newEvent', function(ctx){
-            loadPage(ctx, './templates/userProfile.hbs');
+        this.get('#/newEvent', function (ctx) {
+            loadPage(ctx, './templates/eventsForms/organize.hbs');
         })
-        this.get('#/profile', function(ctx){
+        this.get('#/profile', function (ctx) {
             loadPage(ctx, './templates/userProfile.hbs');
         })
         this.get('#/login', function (ctx) {
@@ -60,6 +60,62 @@ const templatesPaths = {
         this.get('#/logout', function (ctx) {
             sessionStorage.clear();
             ctx.redirect('#/home');
+        })
+        this.get('#/home/:id', function (ctx) {
+            getUserInfo(ctx);
+            get('appdata', `events/${ctx.params.id}`, 'Kinvey')
+                .then(res => {
+                    ctx.name = res.name;
+                    ctx.description = res.description;
+                    ctx.imageURL = res.imageURL;
+                    ctx.dateTime = res.dateTime;
+                    ctx.organizer = res.organizer;
+                    ctx.peopleInterestedIn = res.peopleInterestedIn;
+                    ctx.isCreator = ctx.id === res._acl.creator;
+                    loadPage(ctx, './templates/eventInfo.hbs')
+                })
+                .catch(err => console.log(err))
+        })
+        this.get('#/home/:id/join', function (ctx) {
+            const id = ctx.params.id
+            get('appdata', `events/${id}`, 'Kinvey')
+                .then(res => {
+                    res.peopleInterestedIn += 1;
+                    put('appdata', `events/${id}`, 'Kinvey', res)
+                        .then(res => ctx.redirect(`#/home/${id}`))
+                        .catch(err => console.log(err))
+                })
+                .catch(err => console.log(err))
+        })
+        this.get('#/home/:id/delete', function (ctx) {
+            del('appdata', `events/${ctx.params.id}`, 'Kinvey')
+                .then(console.log)
+                .then(res => ctx.redirect('#/home'))
+                .catch(console.log)
+        })
+        this.get('#/home/:id/edit', function (ctx) {
+            get('appdata', `events/${ctx.params.id}`, 'Kinvey')
+                .then(res => {
+                    ctx.name = res.name;
+                    ctx.description = res.description;
+                    ctx.imageURL = res.imageURL;
+                    ctx.dateTime = res.dateTime;
+                    ctx.organizer = res.organizer;
+                    ctx.peopleInterestedIn = res.peopleInterestedIn;
+                    ctx.eventID = res._id;
+                    console.log(ctx.eventID)
+                })
+                .then(res => loadPage(ctx, './templates/eventsForms/edit.hbs'))
+                .catch(err => console.log(err))
+
+        })
+        this.post('#/home/:id/edit', function (ctx) {
+            console.log(ctx)
+            const { dateTime, description, imageURL, name, organizer, peopleInterestedIn } = ctx.params;
+            put('appdata', `events/${ctx.params.id}`, 'Kinvey', { dateTime, description, imageURL, name, organizer, peopleInterestedIn })
+                .then(console.log)
+                .then(res => ctx.redirect(`#/home/${ctx.params.id}`))
+                .catch(console.log)
         })
         this.post('#/register', function (ctx) {
             ctx.error = '';
@@ -113,6 +169,43 @@ const templatesPaths = {
                     ctx.error = 'Invalid username or password!';
                     loadPage(ctx, './templates/userForms/login.hbs');
                 })
+        })
+        this.post('#/newEvent', function (ctx) {
+            ctx.error = '';
+            ctx.success = '';
+            ctx.loading = false;
+            getUserInfo(ctx)
+            const { name, dateTime, description, imageURL } = ctx.params;
+            const peopleInterestedIn = 0;
+            const organizer = ctx.username;
+            const regex = /\d{1,2} [A-Z][a-z]+/g;
+            if (name.length < 6) {
+                ctx.error = 'The event name should be at least 6 characters long!';
+                loadPage(ctx, './templates/eventsForms/organize.hbs');
+            }
+            else if (!regex.test(dateTime)) {
+                ctx.error = 'The date should be in format "24 February".';
+                loadPage(ctx, './templates/eventsForms/organize.hbs');
+            }
+            else if (description.length < 10) {
+                ctx.error = 'The description should be at least 10 characters long!';
+                loadPage(ctx, './templates/eventsForms/organize.hbs');
+            }
+            else if (!imageURL.startsWith("http://") && !imageURL.startsWith("https://")) {
+                ctx.error = 'The image should start with "http://" or "https://".';
+                loadPage(ctx, './templates/eventsForms/organize.hbs');
+            }
+            else {
+                ctx.loading = true;
+                loadPage(ctx, './templates/eventsForms/organize.hbs');
+                post('appdata', 'events', 'Kinvey', { name, dateTime, description, imageURL, peopleInterestedIn, organizer })
+                    .then(res => ctx.redirect('#/home'))
+                    .catch(err => {
+                        ctx.error = 'Something went wrong! Please try again!';
+                        loadPage(ctx, './templates/eventsForms/organize.hbs');
+                        console.log(err);
+                    })
+            }
         })
     })
     function loadPage(ctx, path) {
